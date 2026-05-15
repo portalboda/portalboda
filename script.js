1a79e573ac5b25e4f0f8db57007e209ff9f994f2
portalboda <portalboda@gmail.com>
2026-05-15 18:43:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+User-agent: *
+Allow: /
+Disallow: /admin/
+Disallow: /*.json$
+
+Sitemap: https://www.portalboda.cl/sitemap.xml
+
+# Crawl-delay para bots
+Crawl-delay: 1
+
+# Específico para Google
+User-agent: Googlebot
+Allow: /
+Crawl-delay: 0
+
+# Específico para Bing
+User-agent: Bingbot
+Allow: /
+Crawl-delay: 1
